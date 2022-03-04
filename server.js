dee93f39cd6a10fd1b4a8204a6456df93d8fcb69
aryankumar <akumar07092000@gmail.com>
2022-03-05 02:27:04 +0530

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
  
-app.get('*', function(req, res) {
+app.get('/api', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
  
